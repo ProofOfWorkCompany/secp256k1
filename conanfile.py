f542aeb82e02add198b9c0aa07906aa53ffe5d98
Max Hora <maxhora.ua@gmail.com>
2021-08-20 00:15:30 +0300

    Conan recipe and test package for Release build.

---
+++ conanfile.py
@@ -0,0 +1,37 @@
+from conans import ConanFile, CMake
+
+
+class SECP256K1Conan(ConanFile):
+    name = "SECP256K1"
+    version = "0.1"
+    license = "<Put the package license here>"
+    author = "<Put your name here> <And your email here>"
+    url = "<Package recipe repository url here, for issues about the package>"
+    description = "<Description of SECP256K1 here>"
+    topics = ("<Put some tag here>", "<here>", "<and here>")
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+    generators = "cmake"
+    exports_sources = "*"
+    requires = "autoconf/2.71", "automake/1.16.3", "libtool/2.4.6"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def build(self):
+        self.run("./autogen.sh")
+        self.run("./configure --disable-debug --disable-dependency-tracking --disable-silent-rules --prefix=$(pwd)")
+        self.run("make install")
+
+    def package(self):
+        self.copy("*.h", dst="include", src="src")
+        self.copy("*.lib", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.dylib*", dst="lib", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["secp256k1"]
