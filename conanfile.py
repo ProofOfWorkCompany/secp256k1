e9c895fe52d39ed56676c9fbfe90aef9f33997ed
Max Hora <maxhora.ua@gmail.com>
2021-08-27 14:15:31 +0300

    Distinct conan packages for shared and static libs.

--- conanfile.py
+++ conanfile.py
@@ -32,11 +32,15 @@ class SECP256K1Conan(ConanFile):
 
     def package(self):
         self.copy("*.h", dst="include", src="include")
-        self.copy("*.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.dylib*", dst="lib", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
+
+        if self.options.shared == False:
+            self.copy("*.a", dst="lib", keep_path=False)
+            self.copy("*.lib", dst="lib", keep_path=False)
+        else:
+            self.copy("*.lib", dst="lib", keep_path=False)
+            self.copy("*.dll", dst="bin", keep_path=False)
+            self.copy("*.dylib*", dst="lib", keep_path=False)
+            self.copy("*.so", dst="lib", keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = ["secp256k1"]
